13202036d696fbefffc575af56bd69f0a1fe09c3
andrew.boren <andrew.boren@gmail.com>
2011-04-02 15:31:45 -0700

    EVParts: Change preference for launcher style to a listpreference to accomodate multiple launcher styles.
    Change-Id: I80b96dd8d16fa37c5ab11c39ee45b28908c34d06

--- src/com/evervolv/EVParts/LauncherParts.java
+++ src/com/evervolv/EVParts/LauncherParts.java
@@ -33,13 +33,11 @@ implements SharedPreferences.OnSharedPreferenceChangeListener , OnClickListener,
 	//private static final int REQUEST_PICK_APPLICATION = 6;
 	//private static final String FARRIGHT_AB = "farrightaction_button";
 	
-	private ListPreference mFLAB;
-	private ListPreference mFRAB;
-	private ListPreference mNumLauncherButtons;
-	private CheckBoxPreference mUseStockLaunchPref;
+
+	private ListPreference mLauncherStylePref;
 	
 	private String[] mAppNames;
-	private static final String LAUNCHER_STOCK = "use_stock_launcher";
+	private static final String LAUNCHER_STYLE = "launcher_style";
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {	
@@ -47,56 +45,13 @@ implements SharedPreferences.OnSharedPreferenceChangeListener , OnClickListener,
 		addPreferencesFromResource(R.xml.launcher_prefs);
 		getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
 		PreferenceScreen prefSet = getPreferenceScreen();
-		//mFLAB = (ListPreference) findPreference("launcher_farlefttab");
-		//mFLAB.setEnabled(false);
-		//mFLAB.setSummary(Settings.System.getString(getContentResolver(), Settings.System.FARLEFT_AB));
-		//mFRAB = (ListPreference) findPreference("launcher_farrightab");
-		//mFRAB.setEnabled(false);
-		//mNumLauncherButtons = (ListPreference) findPreference("launcher_numlauncherbuttons");
-		//mNumLauncherButtons.setEnabled(false);
 	
 		
-		mUseStockLaunchPref = (CheckBoxPreference)prefSet.findPreference(LAUNCHER_STOCK);
-		mUseStockLaunchPref.setChecked(Settings.System.getInt(getContentResolver(), Settings.System.USE_STOCK_LAUNCHER   , 0) != 0);	
-		
-		
-		
-		//PackageManager pm = getPackageManager();
-		//List<PackageInfo> packs = getPackageManager().getInstalledPackages(PackageManager.GET_ACTIVITIES);
-		//int max = packs.size();
-		//mAppNames = new String[max];
-		//for (int i = 0; i < max; i++) {  
-        //   try {
-        //	   Log.d("LauncherParts", "packageName: " + packs.get(i).packageName );
-        //	   mAppNames[i] = packs.get(i).packageName;
-        //   } catch (NullPointerException e) {
-        //       Log.d("LauncherParts", "NullPointerException @: " + i);
-        //   }
-    		   
-        //}  
-     
-		
-		//mFLAB.setDialogTitle("Far left Action Button.");
-		//mFLAB.setEntries(R.array.entries_farleftbutton);
-		//mFLAB.setEntryValues(R.array.entryvalues_farleftbutton);
-		
-		//mFLAB.setEntries(mAppNames);
-		//mFLAB.setEntryValues(mAppNames);
-		//mFLAB.setOnPreferenceChangeListener(this);
-		
+		mLauncherStylePref = (ListPreference) prefSet.findPreference(LAUNCHER_STYLE);
+		mLauncherStylePref.setValueIndex(Settings.System.getInt(getContentResolver(),
+                Settings.System.LAUNCHER_STYLE, 1));
+		mLauncherStylePref.setOnPreferenceChangeListener(this);
     }
-    
-    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-        boolean value;	
-        if (preference == mUseStockLaunchPref) {
-            value = mUseStockLaunchPref.isChecked();
-            Settings.System.putInt(getContentResolver(), Settings.System.USE_STOCK_LAUNCHER, value ? 1 : 0);
-            ActivityManager am = (ActivityManager)getSystemService(Context.ACTIVITY_SERVICE);
-            am.forceStopPackage("com.android.launcher");
-        }
-        return false;
-    }
-	
 
 	@Override
 	public void onClick(View v) {
@@ -106,9 +61,11 @@ implements SharedPreferences.OnSharedPreferenceChangeListener , OnClickListener,
 
 	@Override
 	public boolean onPreferenceChange(Preference preference, Object newValue) {
-        //if (preference == mFLAB) {
-        //	Settings.System.putString(getContentResolver(), Settings.System.FARLEFT_AB, newValue.toString());
-        //}
+        if (preference == mLauncherStylePref) {
+        	Settings.System.putInt(getContentResolver(), Settings.System.LAUNCHER_STYLE, Integer.valueOf((String) newValue));
+            ActivityManager am = (ActivityManager)getSystemService(Context.ACTIVITY_SERVICE);
+            am.forceStopPackage("com.android.launcher");
+        }
         return false;
 	}
 
