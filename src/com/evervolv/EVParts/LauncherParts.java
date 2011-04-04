ae33be82690560156379e4ea6141b62928d02319
andrew.boren <andrew.boren@gmail.com>
2011-04-03 22:50:10 -0700

    EVParts: Fix an issue where the tablet launcher style option still showed up on phones.
    Change-Id: I0ca57608ea114dccf0aa567973571ff78e4fd1be

--- src/com/evervolv/EVParts/LauncherParts.java
+++ src/com/evervolv/EVParts/LauncherParts.java
@@ -47,8 +47,11 @@ public class LauncherParts extends PreferenceActivity implements OnPreferenceCha
 		mLauncherStylePref.setOnPreferenceChangeListener(this);
 		
 		if (!getResources().getBoolean(R.bool.device_is_tablet)) {
-			CharSequence[] mNonTabValues = { "0", "1" };
-			mLauncherStylePref.setEntryValues(mNonTabValues);
+			CharSequence[] mNonTabEntries = { "Stock style", "Evervolv style" };
+			CharSequence[] mNonTabEntryValues = { "0", "1" };
+			mLauncherStylePref.setEntries(mNonTabEntries);
+			mLauncherStylePref.setEntryValues(mNonTabEntryValues);
+
 		}
     }
 
@@ -56,6 +59,7 @@ public class LauncherParts extends PreferenceActivity implements OnPreferenceCha
 	public boolean onPreferenceChange(Preference preference, Object newValue) {
         if (preference == mLauncherStylePref) {
         	Settings.System.putInt(getContentResolver(), Settings.System.LAUNCHER_STYLE, Integer.valueOf((String) newValue));
+        	// Is there another way we can do this? some sort of settings observer in launcher? or maybe its own preference provider?
             ActivityManager am = (ActivityManager)getSystemService(Context.ACTIVITY_SERVICE);
             am.forceStopPackage("com.android.launcher");
         }
